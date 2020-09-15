cf200c8d00d8081728b9019068e4cb3af8b2e9c1
Bingxu Gao <bingxugao@BGX-MBP.local>
2020-09-15 23:06:55 +0800

    add menu

--- report-new.ts
+++ report-new.ts
@@ -3,11 +3,12 @@ import {select} from "d3-selection";
 // component
 import App from "./components/App";
 // first level style
-import "./report-new.css"
+import "./report-new.css";
+import "./menu.css"
 
 // attach app to DOM
 (() => {
-  select('body').insert(App.tagName(), 'script')
+  select('body').insert(App.tagName(), '#menu-container')
       .classed('container', true)
       .style('display', 'block');
 })();
